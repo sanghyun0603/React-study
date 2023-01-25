aa6551986cef4930d0a599e1c3bf5ad2d85650a4
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2023-01-26 01:44:43 +0900

    zustand

--- pj1/src/pages/Detail.js
+++ pj1/src/pages/Detail.js
@@ -1,16 +1,30 @@
 import { useParams } from "react-router-dom";
 import {useEffect, useState } from "react";
-import {useStore} from '../store.js';
+import {useStore, testStore} from '../store.js';
 
 function Detail(){
-    const { bears, increasePopulation, removeAllBears } = useStore(state => state)
-
+    const { bears, increasePopulation, removeAllBears } = useStore(state => state);
+    const { cart, increaseCount } = testStore(state => state);
     return(
         <div>
             <h1>{bears} around here ...</h1>
             <button onClick={increasePopulation}>one up</button>
             <button onClick={removeAllBears}>remove all</button>
             <p>test</p>
+            {
+              cart.map((data,i)=>{
+                return (
+                  <div key = {i}>
+                    <h2>{data.name}</h2>
+                    <h3>{data.count}
+                      <button onClick={()=>{increaseCount(data.id)}}>
+                        +
+                      </button>
+                    </h3>
+                  </div>
+                )
+              })
+            }
         </div>
     )
 }
--- pj1/src/store.js
+++ pj1/src/store.js
@@ -3,8 +3,6 @@ import {create} from 'zustand' // create로 zustand를 불러옵니다.
 const useStore = create(set => ({
   bears: 0,
   increasePopulation: (a) => {
-    console.log(a);  
-    console.log(useStore.bears);
     set(state => ({ bears : state.bears + 1 }));
   },
   removeAllBears: () => {
@@ -12,15 +10,22 @@ const useStore = create(set => ({
     set(state => ({ bears : 0 }));
   }
 }))
-
+function increaseCounts(ca,id) {
+  ca.map((cas)=>{
+    if (id === cas.id) {
+      cas.count += 1;
+    }
+    return cas
+  })
+  return ca
+}
 const testStore = create(set => ({
     cart : [
         {id : 0, name : 'White and Black', count : 2},
         {id : 1, name : 'Grey Yordan', count : 1}
     ],
     increaseCount : (id) => {
-      console.log(id);
-
+      set(state => ({ cart : increaseCounts(state.cart,id)}));
     }
 }))
 
