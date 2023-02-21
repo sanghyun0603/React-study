218d90d2e39f75070b36ab2a50df20a185e72521
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2023-02-21 23:48:50 +0900

    0221 react

--- Typescript/typescript/example.js
+++ Typescript/typescript/example.js
@@ -1,14 +1,13 @@
 var changed = function (nums) {
-    var newnums = nums.map(function (num) {
-        if (typeof num === "string") {
-            var a = Number(num);
-            return Number(num);
-        }
-        else {
-            return num + 1;
-        }
-    });
-    return newnums;
+  var newnums = nums.map(function (num) {
+    if (typeof num === "string") {
+      var a = Number(num);
+      return Number(num);
+    } else {
+      return num + 1;
+    }
+  });
+  return newnums;
 };
 console.log(changed([123, "3"]));
 // var changed = function (nums) {
@@ -21,4 +20,4 @@ console.log(changed([123, "3"]));
 //     }
 //   });
 // };
-// console.log(changed([123, "3", "5", 12]));
+// console.log(changed([123, "3", "5", 12]));daas
